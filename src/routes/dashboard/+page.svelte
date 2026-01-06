77ad27e371b1b117b0a92a490b6da99ccab0b429
yoswer07 <yoswer07@gmail.com>
2026-01-06 19:03:40 -0400

    iphone ws message bug fix

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -139,7 +139,7 @@
 		const encodedMessage = encodeURIComponent(message);
 		const whatsappUrl = `https://wa.me/${phone}?text=${encodedMessage}`;
 
-		window.open(whatsappUrl, '_blank');
+		window.location.href = whatsappUrl;
 	}
 </script>
 
